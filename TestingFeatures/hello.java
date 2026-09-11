f28def1fa696312a22cc53838b54e31ac40037c6
eduvanthrahul-afk <eduvanthrahul@gmail.com>
2026-09-11 19:43:46 +0530

    checked version of branches

--- TestingFeatures/hello.java
+++ TestingFeatures/hello.java
@@ -1,1 +1,8 @@
-hello.java
+class hello{
+    public static void main(String[] args){
+        System.out.println("Hello GitHub!");
+        int x = 0;
+        System.out.println(x);
+        System.err.println("Error message");
+    }
+}
