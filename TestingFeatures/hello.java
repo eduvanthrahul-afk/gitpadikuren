6cd5884b43bd9bdc8e5b8a75380402f9a9844d9f
eduvanthrahul-afk <eduvanthrahul@gmail.com>
2026-09-11 19:40:55 +0530

    played with branches switch branch and create branches

--- TestingFeatures/hello.java
+++ TestingFeatures/hello.java
@@ -1,1 +1,7 @@
-hello.java
+class hello{
+    public static void main(String[] args){
+        System.out.println("Hello GitHub!");
+        int x = 0;
+        System.out.println(x);
+    }
+}
--- TestingFeatures/main.java
+++ TestingFeatures/main.java
@@ -3,5 +3,6 @@ class main{
         System.out.println("Hello GitHub!");
         int x = 0;
         System.out.println(x);
+        System.out.println("i am on feature branch");
     }
 }
