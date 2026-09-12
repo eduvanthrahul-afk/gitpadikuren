d19d28fe8e25c76c1aa5455ba89976cbed30b7fd
eduvanthrahul-afk <eduvanthrahul@gmail.com>
2026-09-12 08:38:27 +0530

    learned about merge conflict

--- TestingFeatures/main.java
+++ TestingFeatures/main.java
@@ -4,5 +4,7 @@ class main{
         int x = 0;
         System.out.println(x);
         System.out.println("i am on test branch");
+        System.out.println("i am checking for conflict" );
+        System.out.println("hahahah");
     }
 }
